6eda494a4670faffd0aba89c46f26788380bd08a
Rodrigo <galdinorodrigo553@gmail.com>
2026-02-21 02:15:06 -0300

    feat: add validations for elections and cadidates

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,3 +1,14 @@
 class Candidate < ApplicationRecord
   belongs_to :election
+
+  validates :name, presence: true
+  validates :election_id, presence: true
+
+  before_save :name_sanitize
+
+  private
+
+  def name_sanitize
+    self.name = self.name.strip.downcase.titleize
+  end
 end
--- app/models/election.rb
+++ app/models/election.rb
@@ -1,2 +1,19 @@
 class Election < ApplicationRecord
+  has_many :candidates, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
+  validates :expiration_at, presence: true
+
+  before_validation :set_expiration_at, on: :create
+  before_save :name_sanitize
+
+  private
+
+  def set_expiration_at
+    self.expiration_at = Time.current + 1.year
+  end
+
+  def name_sanitize
+    self.name = self.name.strip.downcase.titleize
+  end
 end
