932b43639cfa09e269872dec15ad674dfd6ce8ee
Rodrigo <galdinorodrigo553@gmail.com>
2026-02-25 09:33:35 -0300

    fix: docker file in development mode

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_25_104904) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "candidates", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "election_id", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
+    t.index ["election_id"], name: "index_candidates_on_election_id"
+  end
+
+  create_table "elections", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "expiration_at"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "jti", null: false
+    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at"
+    t.string "reset_password_token"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "candidates", "elections"
 end
