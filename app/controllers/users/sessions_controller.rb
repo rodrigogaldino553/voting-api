ae05cfede5ee9e2f61b54bb131542ace8ed8b3f0
Rodrigo <galdinorodrigo553@gmail.com>
2026-02-27 01:08:21 -0300

    fix: GET /login returns 'logged in successfully' because controller dont check if rescource was persisted

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,10 +2,16 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: {
-      message: "Logged in successfully.",
-      user: resource
-    }, status: :ok
+    if resource.persisted?
+      render json: {
+        message: "Logged in successfully.",
+        user: resource
+      }, status: :ok
+    else
+      render json: {
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
   end
 
   def respond_to_on_destroy(_opts = {})
